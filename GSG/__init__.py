c377e50aa682b6b2f7ad39e2c63d09c973bf2680
LastRevenge <breckin.bodnar2@gmail.com>
2026-01-28 01:55:45 -0500

    Update __init__.py

--- GSG/__init__.py
+++ GSG/__init__.py
@@ -1,6 +1,12 @@
-from .gsg_core import GlobalShadowGrid
+# gsg/gsg_core.py
 from gsg.news_feed import NewsFeed
-self.news = NewsFeed()
 
-def add_news(self, message: str):
-    self.news.post(message)
+class GlobalShadowGrid:
+    def __init__(self):
+        self.news = NewsFeed()
+        self.doctrines = {}
+        self.captured_files = []
+
+    def add_news(self, message: str):
+        self.news.post(message)
+
